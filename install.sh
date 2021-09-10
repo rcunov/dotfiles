a9891e2def4c6ec0d05c78d7bf9afa2d959d3214
Remington Cunov <61329286+rcunov@users.noreply.github.com>
2021-09-10 17:39:09 -0500

    Throwing spaghetti at the wall and seeing what sticks

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Initialize yes/no dialog function
-function ask {
+function ask_yes_no {
     while true; do
         read -p "$* [y/n]: " yn
         case $yn in
@@ -13,26 +13,26 @@ function ask {
 }
 
 # Appends aliases to shell-specific config file such as .bashrc or .zshrc
-ask "Would you like to add aliases to your shell config?" && 
+ask_yes_no "Would you like to add aliases to your shell config?" && 
 read -p "What is your shell config file? (ex. .zshrc): " RCFILE &&
 printf "\n" >> $HOME/$RCFILE && cat aliases.txt >> $HOME/$RCFILE;
 
 
 # Moves vim colorscheme file to proper directory and sets it as default
-ask "Would you like to set the vim style?" &&
+ask_yes_no "Would you like to set the vim style?" &&
 mkdir -p ~/.vim/colors &&
 cp jellybeans.vim ~/.vim/colors &&
 echo "colorscheme jellybeans" > ~/.vimrc
 
 # Add my public keys to authorized_keys file
-ask "Would you like to add public keys to ~/.ssh/authorized_keys?" &&
+ask_yes_no "Would you like to add public keys to ~/.ssh/authorized_keys?" &&
 mkdir -p ~/.ssh &&
 cat authorized_keys >> ~/.ssh/authorized_keys
 
 # Add my tmux configuration
-ask "Would you like to set the tmux config?" && cp tmux.conf ~/.tmux.conf
+ask_yes_no "Would you like to set the tmux config?" && cp tmux.conf ~/.tmux.conf
 
 # Install software I like
-ask "Do you have sudo and would you like to install software?" &&
+ask_yes_no "Do you have sudo and would you like to install software?" &&
 read -p "What package manager are you using? (ex. yum | apt ): " PMGR &&
 sudo $PMGR update && sudo $PMGR install $(cat software.txt)
