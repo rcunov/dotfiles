1fe72440d9aa4106f2e63ad03b7ef62ab366cd7f
rcunov <61329286+rcunov@users.noreply.github.com>
2024-05-30 21:16:06 -0500

    have to change dir again

--- nvim-bootstrap.sh
+++ nvim-bootstrap.sh
@@ -7,6 +7,8 @@ then
   cd /tmp
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
   sudo tar -C /opt -xzf nvim-linux64.tar.gz
+  rm nvim-linux64.tar.gz
+  cd -
   echo export PATH="$PATH:/opt/nvim-linux64/bin" >> ~/.bashrc
   echo "may need to source ~/.bashrc"
 fi
