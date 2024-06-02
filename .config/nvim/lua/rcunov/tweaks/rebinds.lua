6c1db1ae0de8b82c0ab54d211c2414007a11e834
rcunov <61329286+rcunov@users.noreply.github.com>
2024-06-02 02:18:45 -0500

    remap netrw movement keys

--- .config/nvim/lua/rcunov/tweaks/rebinds.lua
+++ .config/nvim/lua/rcunov/tweaks/rebinds.lua
@@ -1,7 +1,13 @@
 vim.cmd([[
+  " -> fold
   "nnoremap <Space> za
+  " -> terminal escape
   tnoremap <Esc> <C-\><C-n>
-  "au FileType netrw nmap <> <CR>
+  " -> netrw movement keys
+  au FileType netrw nmap <buffer> h -
+  au FileType netrw nmap <buffer> <left> -
+  au FileType netrw nmap <buffer> l <CR>
+  au FileType netrw nmap <buffer> <right> <CR>
 ]])
 
 vim.keymap.set("n", "<leader>df", vim.cmd.Ex)
