e68aa4dc7849ffb69d8c306c49b62ce5d717f4c5
rcunov <61329286+rcunov@users.noreply.github.com>
2024-06-06 14:04:29 -0500

    add lsp-zero

---
+++ .config/nvim/lua/rcunov/plugins/lsp-zero.lua
@@ -0,0 +1,28 @@
+return {
+  "VonHeikemen/lsp-zero.nvim",
+  dependencies = {
+   "neovim/nvim-lspconfig",
+   "williamboman/mason.nvim",
+   "williamboman/mason-lspconfig.nvim",
+   "hrsh7th/nvim-cmp",
+   "hrsh7th/cmp-buffer",
+   "hrsh7th/cmp-path",
+   "saadparwaiz1/cmp_luasnip",
+   "hrsh7th/cmp-nvim-lsp",
+   "hrsh7th/cmp-nvim-lua",
+   "L3MON4D3/LuaSnip",
+   "rafamadriz/friendly-snippets",
+   { "lukas-reineke/lsp-format.nvim", config = true },
+  },
+  config = function()
+    local lsp = require("lsp-zero")
+    require('lspconfig').gopls.setup({})
+    lsp.preset("recommended")
+    lsp.on_attach(function(client, bufnr)
+      require("lsp-format").on_attach(client, bufnr)
+    end)
+    -- lsp.nvim_workspace()
+    lsp.setup()
+    vim.diagnostic.config { virtual_text = true }
+  end
+}
