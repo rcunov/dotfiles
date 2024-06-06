bb4947744ab928c269f5e14c315654b44888c29f
rcunov <61329286+rcunov@users.noreply.github.com>
2024-06-06 14:01:42 -0500

    currently broken remaps for autocomplete keys

--- .config/nvim/lua/rcunov/tweaks/rebinds.lua
+++ .config/nvim/lua/rcunov/tweaks/rebinds.lua
@@ -8,6 +8,10 @@ vim.cmd([[
   au FileType netrw nmap <buffer> <left> -
   au FileType netrw nmap <buffer> l <CR>
   au FileType netrw nmap <buffer> <right> <CR>
+  "inoremap <expr> <TAB> pumvisible() ? "\<C-y>" : "\<CR>"
+  "inoremap <expr> <Esc> pumvisible() ? "\<C-e>" : "\<Esc>"
+  "inoremap <expr> <C-j> pumvisible() ? "\<C-n>" : "\<Down>"
+  "inoremap <expr> <C-k> pumvisible() ? "\<C-p>" : "\<Up>"
 ]])
 
 vim.keymap.set("n", "<leader>df", vim.cmd.Ex)
